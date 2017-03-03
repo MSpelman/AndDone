8ca9e66a39431f2ac3f7772590f2eb1fcebf1411
MSpelman <michaelspelman@Michaels-MacBook-Pro.local>
2017-03-03 17:33:22 -0600

    Set Up Tabs
    Set up the tabs

--- app/src/main/java/com/example/anddone/TodoActivity.java
+++ app/src/main/java/com/example/anddone/TodoActivity.java
@@ -143,18 +143,20 @@ public class TodoActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 4;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "To Do";
                 case 1:
-                    return "SECTION 2";
+                    return "Calendar";
                 case 2:
-                    return "SECTION 3";
+                    return "Metrics";
+                case 3:
+                    return "Contacts";
             }
             return null;
         }
